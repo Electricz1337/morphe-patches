8d0593c03ce3ed6006774dda8f901133604302c0
brosssh <brosssh@proton.me>
2026-06-25 15:45:24 +0200

    feat(Instagram - Hide suggested content): Add option to hide suggested accounts
    Closes https://github.com/brosssh/morphe-patches/issues/183 and https://github.com/brosssh/morphe-patches/issues/182.

--- patches/src/main/kotlin/app/morphe/patches/instagram/patches/distractionFree/HideSuggestedContentPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/instagram/patches/distractionFree/HideSuggestedContentPatch.kt
@@ -1,5 +1,7 @@
 package app.morphe.patches.instagram.patches.distractionFree
 
+import app.morphe.library.instagram.patches.blockUrl
+import app.morphe.library.instagram.patches.blockUrlBasePatch
 import app.morphe.library.instagram.patches.filterStoriesListPatch
 import app.morphe.library.instagram.patches.overrideMobileConfigBooleanFlag
 import app.morphe.patcher.patch.booleanOption
@@ -35,12 +37,20 @@ val hideSuggestedContentPatch = bytecodePatch(
         description = "Hides highlighted stories from story tray, sponsored stories which have no set expiration."
     )
 
+    val hideSuggestedAccount by booleanOption(
+        key = "hideSuggestedAccount",
+        default = true,
+        title = "Hide suggested accounts",
+        description = "Hides suggested accounts from the list of followers and from other profiles."
+    )
+
     dependsOn(
         overrideMobileConfigBooleanFlag(
             // Hides suggestions in search box
             override = "111509::3" to false // ig_search_ta_nullstate_suggestions::is_android_enabled
         ),
-        filterStoriesListPatch
+        filterStoriesListPatch,
+        blockUrlBasePatch
     )
 
     execute {
@@ -49,5 +59,10 @@ val hideSuggestedContentPatch = bytecodePatch(
         if (hideSuggestedStories == true) filterSuggestedStories()
 
         if (hideHighlightStories == true) filterHighlightedStories()
+
+        if (hideSuggestedAccount == true) blockUrl(
+            "/discover/ayml",
+            "/discover/chaining"
+        )
     }
 }
